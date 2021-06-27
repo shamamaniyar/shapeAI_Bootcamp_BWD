55c29a143555f7d7484bb34991a0a1e30a22cefd
shamamaniyar <86551019+shamamaniyar@users.noreply.github.com>
2021-06-27 14:51:04 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>JavaScript and React.js</h1>
+      <p> A Basic web Dev JavaScript and React.js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
